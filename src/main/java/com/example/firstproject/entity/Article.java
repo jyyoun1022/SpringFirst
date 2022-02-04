708a4b95b6785672736d8e16c7de6f6e88295e8a
jyyoun1022 <jyyoun1022@naver.com>
2022-02-04 23:45:15 +0900

    2일차

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,10 +1,18 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
+@AllArgsConstructor
+@NoArgsConstructor  //디폴스 생성자를 추가
+@ToString
 @Entity // DB가 해당 객체를 인식 가능!
 public class Article {
 
@@ -18,20 +26,8 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
     }
 
 
-}
+
