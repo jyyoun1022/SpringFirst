993efb6032fa2c1bbb568131b1690b8ab6533c3d
jyyoun1022 <jyyoun1022@naver.com>
2022-02-06 20:34:54 +0900

    게시판 만들어 보기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,11 +24,12 @@ public class ArticleController {
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
-    public String newArticleForm(){
+    public String newArticleForm() {
         return "articles/new";
     }
+
     @PostMapping("/articles/create")
-    public String createArticle(ArticleForm form){
+    public String createArticle(ArticleForm form) {
 
         log.info(form.toString());
         //1.DTO를 변환! Entity로 바꿔라!
@@ -36,60 +38,79 @@ public class ArticleController {
         log.info(article.toString());
 
         //2. Repository에게 Entity를 DB안에 저장하게 한다!
-        Article saved=articleRepository.save(article);
+        Article saved = articleRepository.save(article);
 
         log.info(saved.toString());
 
-        return "redirect:/articles/" +saved.getId();
+        return "redirect:/articles/" + saved.getId();
     }
+
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable Long id, Model model){
-        log.info("id =" +id);
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id =" + id);
         //1. id로 데이터를 가져옴(데이터를 가져오는 주체는 리파지토리)
 //        Optional<Article> articleEntity= articleRepository.findById(id);
         Article articleEntity = articleRepository.findById(id).orElse(null);
         //2. 가져온 데이터를 모델에 등록!
-        model.addAttribute("article" , articleEntity);
+        model.addAttribute("article", articleEntity);
         //3. 보여줄 페이지를 설정!
         return "articles/show";
     }
+
     @GetMapping("/articles")
-    public String index(Model model){
+    public String index(Model model) {
         // 1. 모든 article을 가져온다.
-        List<Article> articleEntityList =  articleRepository.findAll();
+        List<Article> articleEntityList = articleRepository.findAll();
         // 2. 가져온 article을 view로 전달한다.
-        model.addAttribute("articleList",articleEntityList);
+        model.addAttribute("articleList", articleEntityList);
         // 3. view페이지를 설정한다.
         return "articles/index";
 
     }
+
     @GetMapping("/articles/{id}/edit")
-    public String edit(@PathVariable Long id,Model model){
+    public String edit(@PathVariable Long id, Model model) {
 
         //수정할 데이터를 보여주어야 한다.
         Article articleEntity = articleRepository.findById(id).orElse(null);
 
         // 모델에 데이터를 등록
-        model.addAttribute("article",articleEntity);
+        model.addAttribute("article", articleEntity);
 
         //뷰 페이지 설정
         return "articles/edit";
     }
+
     @PostMapping("/articles/update")
-    public String update(ArticleForm form){
+    public String update(ArticleForm form) {
         log.info(form.toString());
         // 1.Dto를 Entity로 변환
         Article articleEntity = form.toEntity();
         // 2. Entity를 DB로 저장한다.
         // 2-1 DB의 기존데이터를 가져온다.
-         Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
         // 2-2 기존 데이터가 있다면 값을 갱신한다.
-        if(target != null){
+        if (target != null) {
             articleRepository.save(articleEntity);  //entity가 DB로 갱신된다.
         }
         // 3. 수정 결과 페이지를 리다이렉트 해준다.
-        return "redirect:/articles/"+articleEntity.getId();
+        return "redirect:/articles/" + articleEntity.getId();
     }
 
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttb) {
+        log.info("삭제요청이 들어왔습니다.");
+        // 1. 삭제 대상을 가져온다.
+        Article articleTarget = articleRepository.findById(id).orElse(null);
+        // 2. 대상을 삭제한다.
+        if (articleTarget != null) {
+            articleRepository.delete(articleTarget);
+            rttb.addFlashAttribute("msg","삭제가 완료 되었습니다.");
+        }
+        // 결과 페이지로 리다이렉트 한다.
+        return "redirect:/articles";
     }
+}
+
+
 
